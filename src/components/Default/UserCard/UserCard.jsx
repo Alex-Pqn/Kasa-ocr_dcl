6f6d8a9b1acda02c897351e19079c8c1341f313b
Sokaaa <Alex-Pqn@users.noreply.github.com>
2022-10-19 17:51:34 +0200

    UserAvatar and UserName components moved into UserCard

--- src/components/Default/UserCard/UserCard.jsx
+++ src/components/Default/UserCard/UserCard.jsx
@@ -1,16 +1,13 @@
-import UserAvatar from './../UserAvatar/UserAvatar';
-import UserName from './../UserName/UserName';
-
 import './UserCard.scss';
 
-const UserCard = ({ userName, userAvatarUrl }) => {
+const UserCard = ({ name, avatarUrl }) => {
   return (
     <div className="user-card">
       <div className="user-card__container user-card__name">
-        <UserName userName={userName} />
+        <span>{name}</span>
       </div>
       <div className="user-card__containe user-card__avatar">
-        <UserAvatar userAvatarUrl={userAvatarUrl} />
+        <img src={avatarUrl} alt="" />
       </div>
     </div>
   );
