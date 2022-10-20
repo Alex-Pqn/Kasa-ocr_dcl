9c87227761bf03c663775ce078cfaa92ebfb840c
Sokaaa <Alex-Pqn@users.noreply.github.com>
2022-10-21 00:11:59 +0200

    Carousel component init + style & responsive

--- src/components/Default/Carousel/Carousel.jsx
+++ src/components/Default/Carousel/Carousel.jsx
@@ -1,11 +1,96 @@
+import ArrowRightIcon from '../../Icon/ArrowRight/ArrowRight';
+import ArrowLeftIcon from '../../Icon/ArrowLeft/ArrowLeft';
+
 import './Carousel.scss';
+import { useEffect, useState, useCallback } from 'react';
 
 const Carousel = ({ picturesUrl }) => {
+  let [activeItem, setActiveItem] = useState();
+  let [activeItemIndex, setActiveItemIndex] = useState();
+
+  const items = picturesUrl;
+  const maxItems = items.length;
+  const maxItemIndexes = maxItems - 1;
+
+  const isThereOnlyOneItem = useCallback(() => {
+    return items.length <= 1;
+  }, [items]);
+
+  const staticCarousel = () =>
+    document
+      .getElementsByClassName('carousel')[0]
+      .classList.add('carousel-static');
+
+  // Event: Next Item
+  function nextItemEvent() {
+    const nextItem = items[activeItemIndex + 1];
+    const nextItemIndex = activeItemIndex + 1;
+
+    nextItem ? setNextItem(nextItem, nextItemIndex) : loopFromStart();
+  }
+  // Event: Prev Item
+  function prevItemEvent() {
+    const prevItem = items[activeItemIndex - 1];
+    const prevItemIndex = activeItemIndex - 1;
+
+    prevItem ? setPrevItem(prevItem, prevItemIndex) : loopFromEnd();
+  }
+
+  // Set: Next Item
+  function setNextItem(nextItem, nextItemIndex) {
+    setActiveItem(nextItem);
+    setActiveItemIndex(nextItemIndex);
+  }
+  // Set: Prev Item
+  function setPrevItem(prevItem, prevItemIndex) {
+    setActiveItem(prevItem);
+    setActiveItemIndex(prevItemIndex);
+  }
+
+  // Loop from the start
+  const loopFromStart = useCallback(() => {
+    setActiveItem(items[0]);
+    setActiveItemIndex(0);
+  }, [setActiveItem, setActiveItemIndex, items]);
+
+  // Loop from the end
+  function loopFromEnd() {
+    setActiveItem(items[maxItemIndexes]);
+    setActiveItemIndex(maxItemIndexes);
+  }
+
+  // Event: DOM load for the first time
+  // => Static carousel if less than one item
+  // => Loop carousel from the start
+  useEffect(() => {
+    if (isThereOnlyOneItem()) staticCarousel();
+    loopFromStart();
+  }, [loopFromStart, isThereOnlyOneItem]);
+
   return (
     <div className="carousel">
-      {picturesUrl.map((picture, index) => (
-        <img key={index} src={picture} alt="" />
-      ))}
+      <div className="carousel__item">
+        <img key={activeItemIndex} src={activeItem} alt="" />
+      </div>
+      <div className="carousel__layer">
+        <div className="carousel__layer__controls">
+          <div className="control-prev control">
+            <span onClick={prevItemEvent}>
+              <ArrowLeftIcon />
+            </span>
+          </div>
+          <div className="control-next control">
+            <span onClick={nextItemEvent}>
+              <ArrowRightIcon />
+            </span>
+          </div>
+        </div>
+        <div className="carousel__layer__indicator">
+          <span>
+            {activeItemIndex + 1}/{maxItems}
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
