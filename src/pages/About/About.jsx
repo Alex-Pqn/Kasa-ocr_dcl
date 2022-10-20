42e7ca7f3f5f3cbfe294f20b882a7c29a25ebe6a
Sokaaa <Alex-Pqn@users.noreply.github.com>
2022-10-21 00:16:00 +0200

    About component init

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,37 @@
+import aboutBanner from './../../assets/about-banner.png';
+import DropdownMenu from '../../components/Default/DropdownMenu/DropdownMenu';
+import Banner from '../../components/Default/Banner/Banner';
+
 import './About.scss';
 
-function About() {  
+function About() {
   return (
     <div className="about">
-      A Propos
+      <section>
+        <Banner
+          isExtended={true}
+          bannerUrl={aboutBanner}
+          bannerAlt="Paysage de montagnes"
+        />
+      </section>
+      <section>
+        <DropdownMenu
+          title="Fiabilité"
+          text="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+        />
+        <DropdownMenu
+          title="Respect"
+          text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+        />
+        <DropdownMenu
+          title="Service"
+          text="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+        />
+        <DropdownMenu
+          title="Sécurité"
+          text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+        />
+      </section>
     </div>
   );
 }
