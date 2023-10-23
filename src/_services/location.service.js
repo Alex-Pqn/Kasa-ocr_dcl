42c4344245092c3ae96795bdf19818bb8991844f
Sokaaa <Alex-Pqn@users.noreply.github.com>
2023-10-23 17:58:18 +0200

    Rework location service for Github Pages

--- src/_services/location.service.js
+++ src/_services/location.service.js
@@ -1,49 +1,18 @@
-import { requestOptions } from '../_helpers/request-options';
+import { requestOptions } from '../_helpers/request-options'
 
-const getApiBaseUrl = () => '/logements.json';
+import locations from '../logements.json'
 
 export const locationService = {
   findAll,
   findOne,
-};
+}
 
 async function findAll() {
-  return fetch(getApiBaseUrl(), requestOptions.get())
-    .then((res) => {
-      if (!res.ok)
-        return Promise.reject(
-          `Unable to get locations from ${getApiBaseUrl()}.`
-        );
-
-      return res.json();
-    })
-    .then((data) => data)
-    .catch((err) => {
-      throw err;
-    });
+  return locations
 }
 
 async function findOne(params) {
-  return fetch(getApiBaseUrl(), requestOptions.get())
-    .then((res) => {
-      if (!res.ok)
-        return Promise.reject(
-          `Unable to get the location from ${getApiBaseUrl()}.`
-        );
+  const location = locations.find((location) => location.id === params.id)
 
-      return res.json();
-    })
-    .then((data) => {
-      const location = data.find((location) => location.id === params.id);
-
-      if (!location)
-        return Promise.reject(
-          `Unable to get the location from ${getApiBaseUrl()}.`
-        );
-
-      return location;
-    })
-    .catch((err) => {
-      throw err;
-    });
+  return location
 }
